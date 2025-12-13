85ebd4a69bb96baf8a22faef70dfde8913d82185
ubaydillah1 <ubaydillah1737@gmail.com>
2025-12-13 19:33:55 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap().catch(() => {});
+bootstrap()
+  .then(() => console.log('https://localhost:3000'))
+  .catch((e) => console.error(e));
